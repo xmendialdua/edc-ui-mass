827b358c7ee36fe4191fe99f6201d73da3095291
jalvaro <jalvaro@ikerlan.es>
2025-05-09 12:11:13 +0000

    en los scripts microk8s porque solo con kubectl no iba

--- ui/UI-k8s/delete_k8s.sh
+++ ui/UI-k8s/delete_k8s.sh
@@ -4,16 +4,16 @@
 NAMESPACE="iflex-provider"
 
 # Eliminar recursos en Kubernetes
-kubectl delete -f k8s/rbac.yaml -n $NAMESPACE
-kubectl delete -f k8s/configmap.yaml -n $NAMESPACE
-kubectl delete -f k8s/deployment.yaml -n $NAMESPACE
-kubectl delete -f k8s/service.yaml -n $NAMESPACE
+microk8s kubectl delete -f k8s/rbac.yaml -n $NAMESPACE
+microk8s kubectl delete -f k8s/configmap.yaml -n $NAMESPACE
+microk8s kubectl delete -f k8s/deployment.yaml -n $NAMESPACE
+microk8s kubectl delete -f k8s/service.yaml -n $NAMESPACE
 
 # Eliminar el namespace si ya no tiene recursos asociados
-kubectl get ns $NAMESPACE &> /dev/null
+microk8s kubectl get ns $NAMESPACE &> /dev/null
 if [ $? -eq 0 ]; then
     echo "Eliminando namespace $NAMESPACE..."
-    kubectl delete namespace $NAMESPACE
+    microk8s kubectl delete namespace $NAMESPACE
 else
     echo "El namespace $NAMESPACE no existe."
 fi
--- ui/UI-k8s/deploy_iflex_k8s.sh
+++ ui/UI-k8s/deploy_iflex_k8s.sh
@@ -14,20 +14,20 @@ echo "Subiendo la imagen a Docker Hub..."
 docker push jalvaro8/iflex-ui:latest
 
 # Verificar si el namespace ya existe
-kubectl get namespace iflex-provider > /dev/null 2>&1
+microk8s kubectl get namespace iflex-provider > /dev/null 2>&1
 
 # Si el namespace no existe, se crea
 if [ $? -ne 0 ]; then
     echo "El namespace 'iflex-provider' no existe. Creando..."
-    kubectl create namespace iflex-provider
+    microk8s kubectl create namespace iflex-provider
 else
     echo "El namespace 'iflex-provider' ya existe."
 fi
 
 # Aplicar los archivos YAML
-kubectl apply -f k8s/rbac.yaml -n iflex-provider
-kubectl apply -f k8s/configmap.yaml -n iflex-provider
-kubectl apply -f k8s/deployment.yaml -n iflex-provider
-kubectl apply -f k8s/service.yaml -n iflex-provider
+microk8s kubectl apply -f k8s/rbac.yaml -n iflex-provider
+microk8s kubectl apply -f k8s/configmap.yaml -n iflex-provider
+microk8s kubectl apply -f k8s/deployment.yaml -n iflex-provider
+microk8s kubectl apply -f k8s/service.yaml -n iflex-provider
 
 echo "Despliegue completado en el namespace 'iflex-provider'."
