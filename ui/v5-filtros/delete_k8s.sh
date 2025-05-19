7c4560cdfea7f99621b24c58e7acd2a1285c46e1
jalvaro <jalvaro@ikerlan.es>
2025-05-19 07:32:43 +0000

    UI con filtros para probarla

---
+++ ui/v5-filtros/delete_k8s.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Namespace
+NAMESPACE="iflex-ui"
+
+# Eliminar recursos en Kubernetes
+kubectl delete -f k8s/rbac.yaml -n $NAMESPACE
+kubectl delete -f k8s/configmap.yaml -n $NAMESPACE
+kubectl delete -f k8s/deployment.yaml -n $NAMESPACE
+kubectl delete -f k8s/service.yaml -n $NAMESPACE
+
+# Eliminar el namespace si ya no tiene recursos asociados
+kubectl get ns $NAMESPACE &> /dev/null
+if [ $? -eq 0 ]; then
+    echo "Eliminando namespace $NAMESPACE..."
+    kubectl delete namespace $NAMESPACE
+else
+    echo "El namespace $NAMESPACE no existe."
+fi
